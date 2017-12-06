0aeeb2ac538c04cd3afa02fa48d113216e08c046
Rodrigo Asensio <rasensio@gmail.com>
2017-12-06 10:19:21 -0500

    update index.js to ES6

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})
